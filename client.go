6c3422538dc663968af7d715f290d2918509a43f
Kamus Hadenes <kamushadenes@hyadesinc.com>
2017-05-11 13:29:06 -0300

    .

--- client.go
+++ client.go
@@ -12,11 +12,19 @@ import (
 const (
 	libraryVersion = "0.1"
 	defaultBaseURL = "http://api.pipl.com/search/"
-	v4Version      = "v5"
+	v4Version      = "v4"
 	defaultTimeout = 10 * time.Second
 )
 
 type V4Client struct {
+	ApiKey    string
+	Version   string
+	BaseURL   string
+	UserAgent string
+	Timeout   time.Duration
+}
+
+type V5Client struct {
 	ApiKey    string
 	Version   string
 	BaseURL   string
@@ -33,7 +41,50 @@ func NewV4Client(key string) *V4Client {
 	return c
 }
 
+func NewV5Client(key string) *V4Client {
+	c := &V5Client{}
+	c.ApiKey = key
+	baseURL := defaultBaseURL + "/"
+	c.UserAgent = "go-pipl-client " + libraryVersion
+	c.BaseURL = baseURL
+	return c
+}
+
 func (c *V4Client) request(timeout time.Duration, params map[string]string) (PiplResponse, error) {
+	req, _ := url.Parse(c.BaseURL)
+	p := url.Values{}
+
+	for k, v := range params {
+		p.Add(k, v)
+	}
+	p.Add("key", c.ApiKey)
+
+	// maybe move http client up into struct
+	client := http.Client{
+		Timeout: timeout,
+	}
+	response, err := client.PostForm(req.String(), p)
+	person := PiplResponse{}
+
+	if err != nil {
+		return person, err
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return person, err
+		}
+		if err = json.Unmarshal(contents, &person); err != nil {
+			return person, err
+		}
+		if len(person.Error) > 0 {
+			return person, errors.New(person.Error)
+		}
+		return person, nil
+	}
+}
+
+func (c *V5Client) request(timeout time.Duration, params map[string]string) (PiplResponse, error) {
 	req, _ := url.Parse(c.BaseURL)
 	p := url.Values{}
 
@@ -76,11 +127,29 @@ func (c *V4Client) SearchPerson(p Person, timeout time.Duration, params map[stri
 	return c.request(timeout, params)
 }
 
+func (c *V5Client) SearchPerson(p Person, timeout time.Duration, params map[string]string) (PiplResponse, error) {
+	person, err := json.Marshal(p)
+	if err != nil {
+		return PiplResponse{}, err
+	}
+	params["person"] = string(person)
+	return c.request(timeout, params)
+}
+
 func (c *V4Client) SearchPointer(search_pointer string, timeout time.Duration, params map[string]string) (PiplResponse, error) {
 	params["search_pointer"] = search_pointer
 	return c.request(timeout, params)
 }
 
+func (c *V5Client) SearchPointer(search_pointer string, timeout time.Duration, params map[string]string) (PiplResponse, error) {
+	params["search_pointer"] = search_pointer
+	return c.request(timeout, params)
+}
+
 func (c *V4Client) SearchParams(search_pointer string, timeout time.Duration, params map[string]string) (PiplResponse, error) {
+	return c.request(timeout, params)
+}
+
+func (c *V5Client) SearchParams(search_pointer string, timeout time.Duration, params map[string]string) (PiplResponse, error) {
 	return c.request(timeout, params)
 }
